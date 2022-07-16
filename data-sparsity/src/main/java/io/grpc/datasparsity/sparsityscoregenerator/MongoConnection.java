f41671de4fa23cad809ff6d8fb57500ff51caa44
Sustain Account <sustain@cs.colostate.edu>
2022-07-15 22:18:15 -0600

    comment code

--- data-sparsity/src/main/java/io/grpc/datasparsity/sparsityscoregenerator/MongoConnection.java
+++ data-sparsity/src/main/java/io/grpc/datasparsity/sparsityscoregenerator/MongoConnection.java
@@ -14,12 +14,14 @@ public class MongoConnection {
 
     public MongoConnection() {
 
+        // Connection URI Data
         String username = System.getenv("ROOT_MONGO_USER");
         String password = System.getenv("ROOT_MONGO_PASS");
         String address = "lattice-100";
         String port = "27018";
         String mongoUri = "mongodb://" + username + ":" + password + "@" + address + ":" + port + "/";
 
+        // Connect, handle exceptions
         try {
             this.client = MongoClients.create(mongoUri);
             this.mongoConnection = this.client.getDatabase("sustaindb");
@@ -29,14 +31,26 @@ public class MongoConnection {
         }
     }
 
+    /*
+     * Explicitly closes the MongoDB connection
+     */
     public void closeConnection() {
         this.client.close();
     }
 
+    /*
+     * Retreives a reference to the MongoDB connection
+     * @Returns: Reference to a database
+     */
     public MongoDatabase getMongoConnection() {
         return this.mongoConnection;
     }
 
+    /*
+     * Retreives a reference to a MongoDB collection
+     * @Params: String, the name of a collection
+     * @Returns: Reference to a collection
+     */
     public MongoCollection<Document> getCollection(String collection) {
         return this.mongoConnection.getCollection(collection);
     }
