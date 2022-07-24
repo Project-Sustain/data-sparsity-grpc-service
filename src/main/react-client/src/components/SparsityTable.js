abba76fef5600514d3275e24b5b29df2e3a2e98d
Sustain Account <sustain@cs.colostate.edu>
2022-07-24 16:13:42 -0600

    add datagrid import

--- src/main/react-client/src/components/SparsityTable.js
+++ src/main/react-client/src/components/SparsityTable.js
@@ -2,7 +2,8 @@
 import { React } from 'react'
 import { useTheme } from '@mui/material/styles';
 import { makeStyles } from "@material-ui/core";
-import { Paper, Typography, List, ListItemButton, Stack, ListItemText, ListSubheader } from '@mui/material';
+import { Paper, Typography, List, ListItemButton, Stack, ListItemText, ListSubheader, Table, TableBody, TableCell, TableHead, TableRow, TableContainer } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 
 const useStyles = makeStyles({
   paper: {
@@ -10,7 +11,7 @@ const useStyles = makeStyles({
     padding: "10px",
   },
   listHeader: {
-    margin: "0px"
+    margin: "20px"
   },
   list: {
     margin: "10px",
@@ -46,19 +47,7 @@ export default function SparsityTable(props) {
       return (
         <Paper className={classes.paper} elevation={2}>
           <Typography align='center' variant='h5'>Sparsity Score Table</Typography>
-          <List component="nav" className={classes.list} disablePadding>
-            <ListSubheader disableGutters className={classes.listHeader}>
-              <Stack direction='row' justifyContent='space-between'>
-                <Typography>Monitor Id</Typography>
-                <Typography>Sparsity Score</Typography>
-              </Stack>
-            </ListSubheader>
-            {
-              props.sparsityData.map((siteData, index) => {
-                return getItemButton(siteData, index)
-              })
-            }
-          </List>
+          {returnTable()}
         </Paper>
       );
     }
@@ -68,6 +57,54 @@ export default function SparsityTable(props) {
         <Paper className={classes.paper} elevation={2}>
           <Typography>Sparsity Data Coming...</Typography>
         </Paper>
+      );
+    }
+
+    function returnTable() {
+      return (
+          <TableContainer component={Paper} className={classes.list} >
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Monitor ID</TableCell>
+                  <TableCell>Sparsity Score</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {
+                  props.sparsityData.map((siteData, index) => {
+                    return (
+                      <TableRow
+                        key={index}
+                        onClick={(event) => handleListItemClick(event, index)}
+                      >
+                        <TableCell>{siteData.monitorId}</TableCell>
+                        <TableCell align='right'>{siteData.sparsityScore}</TableCell>
+                      </TableRow>
+                    );
+                  })
+                }
+              </TableBody>
+            </Table>
+          </TableContainer>
+      );
+    }
+
+    function returnList() {
+      return (
+        <List component="nav" className={classes.list} disablePadding>
+          <ListSubheader disableGutters>
+            <Stack direction='row' justifyContent='space-between'>
+              <Typography className={classes.listHeader}>Monitor Id</Typography>
+              <Typography className={classes.listHeader}>Sparsity Score</Typography>
+            </Stack>
+          </ListSubheader>
+          {
+            props.sparsityData.map((siteData, index) => {
+              return getItemButton(siteData, index)
+            })
+          }
+        </List>
       );
     }
 }
