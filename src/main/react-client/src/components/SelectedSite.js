530a3da5a64b3d9ad061200784b396b0c2f93081
Sustain Account <sustain@cs.colostate.edu>
2022-07-24 15:48:03 -0600

    add relative sparsity score

--- src/main/react-client/src/components/SelectedSite.js
+++ src/main/react-client/src/components/SelectedSite.js
@@ -1,28 +1,52 @@
 import { makeStyles } from "@material-ui/core";
-import { Paper, Typography } from '@mui/material';
+import { Divider, Paper, Typography, List, ListItemText, Stack } from '@mui/material';
+import moment from 'moment';
+// import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const useStyles = makeStyles({
     paper: {
         margin: "10px",
         padding: "10px",
-        maxHeight: "70vh",
-        maxWidth: "40vw",
-        overflow: "auto"
-    }
+    },
+    list: {
+        padding: '10px',
+        margin: '10px',
+        maxHeight: '25vh',
+        overflow: 'auto'
+    },
+    listItem: {
+        margin: '10px'
+    },
 });
 
 export default function SelectedSite(props) {
     const classes = useStyles();
     if(props.site) {
+        const dates = props.site.epochTimes.map((epoch) => {
+            return moment.unix(epoch/1000).format('MM/DD/YYYY HH:mm:ss');
+        })
         return (
             <Paper className={classes.paper}>
-                <Typography>Monitor ID: {JSON.stringify(props.site.monitorId)}</Typography>
-                <Typography>Formal Name: {JSON.stringify(props.site.organizationFormalName)}</Typography>
-                <Typography>Site Type: {JSON.stringify(props.site.monitoringLocationTypeName)}</Typography>
-                <Typography>Score: {props.site.sparsityScore}</Typography>
-                <Typography>Number of Measurements: {props.site.numberOfMeasurements}</Typography>
-                <Typography>Measurement Times: {JSON.stringify(props.site.epochTimes)}</Typography>
-                <Typography>Coordinates: {props.site.coordinates.latitude}, {props.site.coordinates.longitude}</Typography>
+                <Typography variant='h5' align='center'>Info for Site {props.site.monitorId}</Typography>
+                <Stack direction='row' justifyContent='space-around' >
+                    <Paper className={classes.paper}>
+                        <Typography><strong>Formal Name:</strong> {props.site.organizationFormalName}</Typography>
+                        <Typography><strong>Site Type:</strong> {props.site.monitoringLocationTypeName}</Typography>
+                        <Typography><strong>Absolute Sparsity Score:</strong> {props.site.sparsityScore}</Typography>
+                        <Typography><strong>Relative Sparsity Score:</strong> {props.site.relativeSparsityScore}</Typography>
+                    </Paper>
+                    <Paper className={classes.paper}>
+                        <Typography align='center' variant='h6'>{props.site.numberOfMeasurements} Measurements</Typography>
+                        <List className={classes.list}>
+                            {dates.map((date, index) => {
+                                return <ListItemText className={classes.listItem} key={index}>{date}</ListItemText>
+                            })}
+                        </List>
+                    </Paper>
+
+                </Stack>
+                {/* <Typography>Coordinates: {props.site.coordinates.latitude}, {props.site.coordinates.longitude}</Typography> */}
+                <Divider textAlign="left">Data Samples Over Time</Divider>
             </Paper>
         );
     }
