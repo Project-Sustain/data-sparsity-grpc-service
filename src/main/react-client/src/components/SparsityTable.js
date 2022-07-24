8ebb392ae413047f5e7894a9c9516db99e791148
Sustain Account <sustain@cs.colostate.edu>
2022-07-24 16:56:43 -0600

    unused imports

--- src/main/react-client/src/components/SparsityTable.js
+++ src/main/react-client/src/components/SparsityTable.js
@@ -2,7 +2,7 @@
 import { React } from 'react'
 import { useTheme } from '@mui/material/styles';
 import { makeStyles } from "@material-ui/core";
-import { Paper, Typography, List, ListItemButton, Stack, ListItemText, ListSubheader, Table, TableBody, TableCell, TableHead, TableRow, TableContainer } from '@mui/material';
+import { Paper, Typography, Stack } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 
 const useStyles = makeStyles({
@@ -24,24 +24,6 @@ const useStyles = makeStyles({
 export default function SparsityTable(props) {
     const theme = useTheme();
     const classes = useStyles(theme);
-
-    const handleListItemClick = (event, index) => {
-      props.setSelectedIndex(index);
-    };
-    
-    function getItemButton(siteData, index) {
-      const sparsityScore = siteData.sparsityScore;
-      return(
-        <ListItemButton
-          key={index}
-          selected={props.selectedIndex === index}
-          onClick={(event) => handleListItemClick(event, index)}
-        >
-          <ListItemText primary={siteData.monitorId} />
-          <Typography edge='end'>{sparsityScore}</Typography>
-        </ListItemButton>
-      );
-    }
 
     if(props.sparsityData.length > 0) {
       return (
